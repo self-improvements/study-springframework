ab9ceb498fb84197a214a0a0bdae7e16d34d71d8
ImSejin <hackeres123@gmail.com>
2021-03-29 15:20:41 +0900

    ✅ Study: application properties

--- src/main/java/io/github/imsejin/springstudy/Application.java
+++ src/main/java/io/github/imsejin/springstudy/Application.java
@@ -2,8 +2,10 @@ package io.github.imsejin.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/test.properties")
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/test/java/io/github/imsejin/springstudy/property/PropertyTest.java
@@ -0,0 +1,59 @@
+package io.github.imsejin.springstudy.property;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RequiredArgsConstructor
+class PropertyTest {
+
+    private final ApplicationContext context;
+
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${who.am.i}")
+    private String whoAmI;
+
+    /**
+     * {@link org.springframework.context.annotation.PropertySource} 애노테이션을 사용하여
+     * 아래의 코드처럼 default {@code application.properties}뿐 아니라 다른 properties도 불러올 수 있다.
+     *
+     * <pre>{@code
+     *     @SpringBootApplication
+     *     @PropertySource("classpath:/test.properties")
+     *     public class Application {
+     *         public static void main(String[] args) {
+     *             SpringApplication.run(Application.class, args);
+     *         }
+     *     }
+     * }</pre>
+     *
+     * <p> {@code JVM System Properties}(e.g. -Dkey="value")로 지정한 property가
+     * properties file보다 우선순위가 높다.
+     */
+    @Test
+    void test() {
+        // given
+        Environment environment = context.getEnvironment();
+
+        // when
+        String appName = environment.getProperty("app.name"); // application.properties
+        String whoAmI = environment.getProperty("who.am.i"); // test.properties
+
+        // then
+        assertThat(appName)
+                .isNotNull()
+                .isEqualTo(this.appName);
+        assertThat(whoAmI)
+                .isNotNull()
+                .isEqualTo(this.whoAmI);
+    }
+
+}
