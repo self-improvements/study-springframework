7a95168c5f7c05ec32b80b28c5789b25684cf164
imsejin <hackeres123@gmail.com>
2023-11-24 15:23:15 +0900

    test: fix test failure

--- spring-core/src/main/java/io/github/imsejin/study/springframework/core/validation/model/Lens.java
+++ spring-core/src/main/java/io/github/imsejin/study/springframework/core/validation/model/Lens.java
@@ -3,11 +3,9 @@ package io.github.imsejin.study.springframework.core.validation.model;
 import io.github.imsejin.study.springframework.core.validation.constraint.AnyOf;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -15,7 +13,6 @@ import java.time.LocalDate;
 @AnyOf(constraint = NotEmpty.class, fieldNames = {"modelName", "brandName"})
 public class Lens {
 
-    @Pattern(regexp = "[A-Z]{0,4}")
     private Long serialNumber;
 
     private String modelName;
